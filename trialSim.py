0865d0f8f74344fcf495fbe1ba1ed13245d0f977
Nathan Taylor <taylorbn@alumni.unc.edu>
2019-04-03 09:47:04 -0400

    Updated params, data compilation for offline analysis

--- trialSim.py
+++ trialSim.py
@@ -2,12 +2,12 @@ from brian2 import *
 from matplotlib import *
 import numpy as np
 import random
-from fractions import Fraction
+
 seed(451)
 ##############
 # Parameters #
 ##############
-Condition = 'Control' # Control, GABAzine, ACh
+Condition = 'Control' # control, GABAzine
 # Cell numbers to reproduce functionally relevant 100um^3 cube of granular layer
 nmf  =  315                 # Mossy fibers
 nGrC = 4096                 # Granule cells
@@ -30,7 +30,7 @@ E_l_GrC   = -75   * mV
 E_e_GrC   =   0   * mV
 E_i_GrC   = -75   * mV
 
-E_l_GoC   = -50   * mV
+E_l_GoC   = -49.99   * mV
 E_e_GoC   =   0   * mV
 E_i_GoC   = -75   * mV
 
@@ -39,10 +39,10 @@ C_m_GrC   =  3.1  * pF
 C_m_GoC   = 60.   * pF
 
 # TODO Use rise and decay times and add dyanmics to neuron Equations
-tau_e_decay_GrC = 6.0 * ms
-tau_e_decay_GoC = 3.0 * ms
+tau_e_decay_GrC = 8.0 * ms
+tau_e_decay_GoC = 2.0 * ms
 
-tau_i_decay_GrC = 15.0 * ms
+tau_i_decay_GrC = 8.0 * ms
 
 # Absolute refractory period
 tau_r = 2 * ms
@@ -56,19 +56,16 @@ V_r_GrC    = -75 * mV
 V_r_GoC    = -55 * mV
 
 # Golgi cell reset potential
-V_reset_GoC = -60 * mV
+V_reset_GoC = -65 * mV
 
 # Synaptic weights
-w_e_GrC = 0.65 * nS
-w_i_GrC = 0.08 * nS
+w_e_GrC = 0.22 * nS
+w_i_GrC = 0.43 * nS
 
-w_e_GoC_M = 0.35  * nS
-w_e_GoC_GrC = 0.0 * nS
+w_e_GoC = 0.3  * nS
 
-# Stochastic fluctuating excitatory current
-sigma_n_GoC = 0.01 * nS
-sigma_n_GrC = 0.03 * nS
-
+# Golgi cell stochastic fluctuating excitatory current
+sigma_n = 0.05 * nS
 tau_n   = 20 * ms
 
 #############
@@ -77,7 +74,7 @@ tau_n   = 20 * ms
 
 GrC_eqs = '''
 dv/dt   = (g_l*(E_l-v) + (g_e+g_n)*(E_e-v) + (g_i+g_t)*(E_i-v))/C_m : volt (unless refractory)
-dg_n/dt = (-g_n + sigma_n_GrC * sqrt(tau_n) * xi)/tau_n : siemens
+dg_n/dt = (-g_n + sigma_n * sqrt(tau_n) * xi)/tau_n : siemens
 dg_e/dt = -g_e/tau_e : siemens
 dg_i/dt = -g_i/tau_i : siemens
 '''
@@ -85,7 +82,7 @@ dg_i/dt = -g_i/tau_i : siemens
 GoC_eqs = '''
 dv/dt   = (g_l*(E_l-v) + (g_e+g_n)*(E_e-v))/C_m : volt (unless refractory)
 dg_e/dt = -g_e/tau_e : siemens
-dg_n/dt = (-g_n + sigma_n_GoC * sqrt(tau_n) * xi)/tau_n : siemens
+dg_n/dt = (-g_n + sigma_n * sqrt(tau_n) * xi)/tau_n : siemens
 '''
 
 #####################
@@ -98,8 +95,6 @@ mf_indices = np.arange(nmf)
 n_active   = round(nmf/20)           # Fraction of mfs active at each stimulation
 
 # Randomly select a subset of mossy fibers to spike at each stimulation
-# If you want a different subset of mossy fibers at each stimulation, move
-# the declaration of [active_indices] into the loop over stim_times
 random.seed(a=451)
 active_indices = [mf_indices[i] for i in sorted(random.sample(range(len(mf_indices)),n_active))]
 indices    = []
@@ -118,7 +113,7 @@ if Condition == 'GABAzine':
     GrC = NeuronGroup(nGrC,
                       Equations(GrC_eqs,
                                 g_l   = g_l_GrC,
-                                g_t   = 0*nS,
+                                g_t   = 0 * nS,
                                 E_l   = E_l_GrC,
                                 E_e   = E_e_GrC,
                                 E_i   = E_i_GrC,
@@ -146,24 +141,6 @@ elif Condition == 'Control':
                   refractory = 'tau_r',
                   method     = 'euler')
     GrC.v   = V_r_GrC
-elif Condition == 'ACh':
-    GrC = NeuronGroup(nGrC,
-                    Equations(GrC_eqs,
-                        g_l   = g_l_GrC,
-                        g_t   = 0.5 * g_t_GrC,
-                        E_l   = E_l_GrC,
-                        E_e   = E_e_GrC,
-                        E_i   = E_i_GrC,
-                        C_m   = C_m_GrC,
-                        tau_e = tau_e_decay_GrC,
-                        tau_i = tau_i_decay_GrC),
-              threshold  = 'v > V_th_GrC',
-              reset      = 'v = V_r_GrC',
-              refractory = 'tau_r',
-              method     = 'euler')
-    GrC.v   = V_r_GrC
-else:
-    print('ERROR: Unknown experimental condition')
 
 GoC = NeuronGroup(nGoC,
                   Equations(GoC_eqs,
@@ -182,42 +159,21 @@ GoC.v = V_r_GoC
 ###################
 # Connect neurons #
 ###################
-if Condition in ('Control', 'GABAzine'):
-    # Mossy fiber onto GrCs
-    GrC_M = Synapses(mf_input,GrC,
-                     on_pre = 'g_e += w_e_GrC')
-    GrC_M.connect(p = conv_GrC_M/nmf)
 
-    # Mossy fiber onto GoCs
-    GoC_M = Synapses(mf_input,GoC,
-                     on_pre = 'g_e += w_e_GoC_M')
-    GoC_M.connect(p = conv_GoC_M/nmf)
+# Mossy fiber onto GrCs
+GrC_M = Synapses(mf_input,GrC,
+                 on_pre = 'g_e += w_e_GrC')
+GrC_M.connect(p = conv_GrC_M/nmf)
 
-    # GrC onto GoCs
-    GoC_GrC = Synapses(GrC,GoC,
-                       on_pre = 'g_e += w_e_GoC_GrC')
-    GoC_GrC.connect(p = conv_GoC_GrC/nGrC)
-elif Condition == 'ACh':
-    # Mossy fiber onto GrCs
-    GrC_M = Synapses(mf_input,GrC,
-                     on_pre = 'g_e += 0.4521*w_e_GrC')
-    GrC_M.connect(p = conv_GrC_M/nmf)
+# Mossy fiber onto GoCs
+GoC_M = Synapses(mf_input,GoC,
+                 on_pre = 'g_e += w_e_GoC')
+GoC_M.connect(p = conv_GoC_M/nmf)
 
-    # Mossy fiber onto GoCs
-    GoC_M = Synapses(mf_input,GoC,
-                     on_pre = 'g_e += 0.4578*w_e_GoC_M')
-    GoC_M.connect(p = conv_GoC_M/nmf)
-
-    # GrC onto GoCs
-    GoC_GrC = Synapses(GrC,GoC,
-                       on_pre = 'g_e += 0.4578*w_e_GoC_GrC')
-    GoC_GrC.connect(p = conv_GoC_GrC/nGrC)
-
-    # GoC onto GrC (inhibitory)
-    GrC_GoC = Synapses(GoC,GrC,
-                       on_pre = 'g_i += 0.013 * w_i_GrC',
-                       delay = tau_r)
-    GrC_GoC.connect(p = conv_GrC_GoC/nGoC)
+# GrC onto GoCs
+GoC_GrC = Synapses(GrC,GoC,
+                   on_pre = 'g_e += w_e_GoC')
+GoC_GrC.connect(p = conv_GoC_GrC/nGrC)
 
 # GoC onto GrC (inhibitory)
 if Condition == 'GABAzine':
@@ -234,73 +190,54 @@ GrC_GoC.connect(p = conv_GrC_GoC/nGoC)
 ##############
 # Simulation #
 ##############
-
-# Monitor output
-spikes_GrC = SpikeMonitor(GrC)
+M = SpikeMonitor(GrC)
 state_GrC  = StateMonitor(GrC, 'v', record = True)
-conde_GrC  = StateMonitor(GrC_M,'g_e', record = True)
-condi_GrC  = StateMonitor(GrC_GoC,'g_i', record = True)
-LFP        = PopulationRateMonitor(GrC)
-spikes_GoC = SpikeMonitor(GoC)
-state_GoC  = StateMonitor(GoC, 'v', record = True)
-conde_GoC  = StateMonitor(GoC_M,'g_e', record = True)
+store('Control')
 
-# Run simulation
-runtime = 2000
-run(runtime * ms)
+spikes = []
+for trial in range(2):
+    print('Trial %d' % (trial+1))
+    restore('Control')
 
-spikes = spikes_GrC.spike_trains()
-row = np.concatenate([[key]*len(val) for key,val in spikes.items()])
-num_active = len(np.unique(row))
-fraction_active = Fraction(num_active,nGrC)
-print('Active fraction: ', fraction_active)
+    # Run simulation
+    runtime = 2000
+    run(runtime * ms)
 
-# Plots
-fig, ax = plt.subplots(2, 3, figsize=(9, 9))
-for g in range(20):
-    ax[0,0].plot(state_GrC.t/ms, state_GrC.v[g]/mV)
-ax[0,0].set_title('GrC Vm (population)')
-ax[0,0].set_xlabel('time (ms)')
-ax[0,0].set_ylabel('Vm (mV)')
+    # Plotting membrane potential traces for each trial to make sure the
+    # stochastic current is initialized differently with each network
+    # instantiation
+    plt.figure()
+    for g in range(20):
+        plot(state_GrC.t/ms, state_GrC.v[g]/mV)
+    xlim((1000,1040))
 
-ax[0,1].plot(spikes_GrC.t/ms,spikes_GrC.i,'.k')
-for stim in range(nstim):
-    ax[0,1].axvline(stim_times[stim], ls = '-', color = 'r', lw = 2)
-ax[0,1].set_xlim(0,runtime)
-ax[0,1].set_ylim(0,nGrC)
-ax[0,1].set_title('GrC population activity')
-ax[0,1].set_xlabel('time (ms)')
-ax[0,1].set_ylabel('GrC index')
+    # Save spike times for each simulation
+    spike_trains = M.spike_trains()
+    spikes.append(spike_trains)
 
-for g in range(20):
-    ax[0,2].plot(state_GrC.t/ms, conde_GrC.g_e[g]*(-70*mV-E_e_GrC)/pA)
-    ax[0,2].plot(state_GrC.t/ms, condi_GrC.g_i[g]*(10*mV-E_i_GrC)/pA)
-ax[0,2].set_title('GrC EPSC/IPSC')
-ax[0,2].set_xlabel('time (ms)')
-ax[0,2].set_ylabel('I (pA)')
-ax[0,2].set_ylim(-60,60)
+############
+# Analysis #
+############
+import pandas as pd
+import scipy.sparse
+dt     = 1e-4
+mats   = []
+tuples = []
+for trial_idx  in range(2):
+    nrow    = len(spikes[trial_idx])
+    arrays  = [np.full(nrow,trial_idx),range(nrow)]
+    tuples += list(zip(*arrays))
 
-for g in range(nGoC):
-    ax[1,0].plot(state_GoC.t/ms, state_GoC.v[g]/mV)
-ax[1,0].set_title('GoC Vm (population)')
-ax[1,0].set_xlabel('time (ms)')
-ax[1,0].set_ylabel('Vm (mV)')
+    row  = np.concatenate([[key]*len(val) for key,val in spikes[trial_idx].items()])
+    data = np.concatenate([val for key,val in spikes[trial_idx].items()])
+    col  = data/dt
+    ncol = int(runtime*10**-3/dt)
+    trial_mat  = scipy.sparse.csr_matrix((data,(row.astype(int), col.astype(int))),shape = (nrow,ncol))
+    mats.append(trial_mat)
 
-ax[1,1].plot(spikes_GoC.t/ms,spikes_GoC.i,'.k')
-ax[1,1].set_xlim(0,runtime)
-ax[1,1].set_ylim(0,nGoC)
-ax[1,1].set_title('GoC population activity')
-ax[1,1].set_xlabel('time (ms)')
-ax[1,1].set_ylabel('GoC index')
+mat = scipy.sparse.vstack(mats)
+multi_index = pd.MultiIndex.from_tuples(tuples,names=['trial','neuron'])
+df = pd.SparseDataFrame(mat,index=multi_index)
 
-for g in range(nGoC):
-    ax[1,2].plot(state_GoC.t/ms, conde_GoC.g_e[g]*(-60*mV-E_e_GrC)/pA)
-ax[1,2].set_title('GoC EPSC')
-ax[1,2].set_xlabel('time(ms)')
-ax[1,2].set_ylabel('I (pA)')
-ax[1,2].set_ylim(-140,5)
-
-plt.figure()
-plot(LFP.t/ms, LFP.smooth_rate(window='flat', width=0.5*ms)/Hz)
 
 show()
